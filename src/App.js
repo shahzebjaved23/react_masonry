805567ad42d57220a16d73b4f57cbe6c29a2c8fc
shahzebjaved23 <shahzebjaved23@gmail.com>
2018-04-03 23:10:36 +0500

    fixes for card resizing issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from "./Card";
-import Masonry from 'react-masonry-component';
+import Masonry  from 'react-masonry-component';
+import $ from 'jquery';
+
 
 class App extends Component {
     render() {
         return (
-            <Masonry
-                className={'my-gallery-class'} 
-                elementType={'div'}  
-                disableImagesLoaded={false}
-                updateOnEachImageLoad={false}>
-
-                    {this.props.elements.map((element)=>{
-                        return (
-                               <Card src={element.src} text={element.text} />
-                        )
-                    })}
+            <Masonry enableResizableChildren={true} className={'my-gallery-class'} elementType={'div'} disableImagesLoaded={false} updateOnEachImageLoad={false}>
+                {this.props.elements.map((element,index)=>{
+                    return (
+                        <Card key={index} src={element.src} text={element.text} />
+                    )
+                })}
             </Masonry>
         );
     }
